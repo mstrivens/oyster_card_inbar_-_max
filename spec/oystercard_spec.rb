8500aad62b7e199a2f481af92d61835955f5f81b
mstrivens <max.strivens@live.co.uk>
2021-03-08 15:22:16 +0000

    Top-up method tested and created.
    Co-authored-by: Inbar Vernia  <78118133+inbarvernia@users.noreply.github.com>

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,10 +2,27 @@ require 'oystercard'
 
 describe Oystercard do
   let(:oystercard) { Oystercard.new }
-  
+
   describe '#balance' do
     it 'set to 0 by default' do
       expect(oystercard.balance).to eq(0)
     end
+  end
+
+  describe '#top_up' do
+
+    context 'given 5 as amount' do
+      it 'tops up balance by 5' do
+        expect { oystercard.top_up(5) } .to change{
+          oystercard.balance }.by 5
+      end
+    end
+
+    context 'given 10 as amount' do
+      it 'tops up balance by 10' do
+        expect { oystercard.top_up(10) } .to change{
+          oystercard.balance }.by 10
+        end
+      end
   end
 end
