34604769a09b3224095e64b14ca0988f140152f8
inbarvernia <78118133+inbarvernia@users.noreply.github.com>
2021-03-08 15:01:49 +0000

    Added balance attribute and default balance of 0
    Co-authored-by: Max Strivens <74908625+mstrivens@users.noreply.github.com>

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,11 @@
 require 'oystercard'
 
 describe Oystercard do
-
+  let(:oystercard) { Oystercard.new }
+  
+  describe '#balance' do
+    it 'set to 0 by default' do
+      expect(oystercard.balance).to eq(0)
+    end
+  end
 end
